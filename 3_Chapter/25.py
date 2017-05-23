dc140c15202f64b0f55ead367c8c22bbc8c4e3b6
sur1910 <onenight1910@gmail.com>
2017-05-23 14:14:22 +0900

    update 25

--- 3_Chapter/25.py
+++ 3_Chapter/25.py
@@ -2,30 +2,15 @@ import re
 
 
 if __name__ == '__main__':
-    f = open('./jawiki-country.txt', 'r').readlines()
+    f = open('./jawiki-country.txt').read()
+    pattern = re.compile(r"{{基礎情報 国\n(.*)\n}}", re.S)
+    matchOB = '\n'.join(pattern.findall(f))
+    pattern = re.compile(r"\|(.*) = (.*)")
+    ans = pattern.findall(matchOB)
 
     dict = {}
-
-    for i in range(len(f)):
-        if "基礎情報" in f[i]:
-            for j in range(i+1, len(f)):
-                s = ''
-                key = ''
-                data = f[j].split()
-                if data == []:
-                    break
-                if data[0][0] == '|':
-                    n = 0
-                    for k in range(len(data)):
-                        n += 1
-                        if data[k] == "=":
-                            break
-                        key += data[k]
-                    for l in range(n, len(data)):
-                        s += data[l]
-                    dict[key[1:]] = s
-        if f[i][0] == '}':
-            break
-        for i in dict:
-            print(i, dict[i])
+    for i in ans:
+        print(i[0] + ' = ' + i[1])
+        dict[i[0]] = i[1]
+    print(dict)
 
