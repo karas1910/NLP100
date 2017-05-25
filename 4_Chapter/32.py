d65e5bf30dbcb2ee518a523698eaee00c5740cee
sur1910 <onenight1910@gmail.com>
2017-05-25 16:03:34 +0900

    add 32 and 33

---
+++ 4_Chapter/32.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    with open("./neko.txt.mecab") as f:
+        sentenceL = [s for s in f.read().split('EOS\n') if s != '']
+        result = []
+
+        for s in sentenceL:
+            tmpL = []
+            x  = s[:-1].split('\n')
+            for l in x:
+                surface, tail = l.split('\t')
+                sp = tail.split(',')
+                tmpL.append({'surface': surface, 'base': sp[6], 'pos': sp[0], 'pos1': sp[1]})
+            result.append(tmpL)
+
+        for i in result:
+            if i[0]['pos'] == '動詞':
+                print(i[0]['base'])
