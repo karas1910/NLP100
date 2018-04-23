f811f30504dc7fca689eab341a589b9ec263d62a
sur1910 <https://github.com/sur1910>
2018-04-23 16:09:51 +0900

    add 22

---
+++ 3_Chapter/22.py
@@ -0,0 +1,12 @@
+import re
+
+
+def getc_name(target):
+    p = re.compile('\[\[Category:(.*)(\|?\*?\]\])')
+    return [m.group(1) for m in (p.match(l) for l in target) if m != None]
+
+
+if __name__ == '__main__':
+    target = open('./jawiki-country.txt').readlines()
+    result = getc_name(target)
+    [print(l) for l in result]
