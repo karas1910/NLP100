596a2546e80392002e007605b4cd52c8c8a76bd9
sur1910 <onenight1910@gmail.com>
2018-05-01 19:41:11 +0900

    add 28

---
+++ 3_Chapter/n28.py
@@ -0,0 +1,18 @@
+import re
+from n25 import get_basic_info
+from n26 import remove_simbol
+from n27 import remove_link_simbol
+
+
+def remove_markups(dict):
+    p1 = re.compile('\{\{(.*\||)(.*)\}\}')
+    for k, v in dict.items():
+        dict[k] = p1.sub(r'\2', dict[k])
+    return dict
+
+
+if __name__ == '__main__':
+    target = open('./jawiki-country.txt').read()
+    target = remove_simbol(get_basic_info(target))
+    dict = remove_markups(remove_link_simbol(target))
+    [print('{0} = {1}'.format(k, v)) for k, v in dict.items()]
