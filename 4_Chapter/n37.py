9759af1b29c807128c2a6ce5e1bb48a786d692dc
sur1910 <onenight1910@gmail.com>
2018-05-07 16:44:21 +0900

    add 37

---
+++ 4_Chapter/n37.py
@@ -0,0 +1,22 @@
+from n30 import sentence_mapping
+from n36 import get_feq_word
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    with open('./neko.txt.mecab') as f:
+        num = int(input('Input number(0: all): '))
+        sentenceL = [s for s in f.read().split('EOS\n') if s != '']
+        target = sentence_mapping(sentenceL)
+        result, num = get_feq_word(target, num)
+
+        x = range(num)
+        ch = [ch[0] for ch in result]
+        feq = np.array([ch[1] for ch in result])
+
+        plt.hist(feq, log=True)
+        plt.title('Histgram')
+        plt.xlabel('feq')
+        plt.ylabel('Vocabulary')
+        plt.show()
