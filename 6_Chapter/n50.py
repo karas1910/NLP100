b5150755392803ba0fc42d022bea19bb94361abd
sur1910 <onenight1910@gmail.com>
2018-05-25 23:04:26 +0900

    add 50

---
+++ 6_Chapter/n50.py
@@ -0,0 +1,27 @@
+#
+# usage: python n51.py {file name}
+#
+
+
+import re
+import sys
+
+
+def create_sentence_list(fdata):
+    split_pattern, sub_pattern = (re.compile('([.;:?!])\s([A-Z])'),
+                                  re.compile('\n+'))
+    fdata_strip = sub_pattern.sub(' ', fdata)
+    fdata_split = split_pattern.split(fdata_strip)
+    tmp = [''] + [s.strip() for s in fdata_split] + ['']
+    # '[A-Z] or ""' + 'sentence' + '. or ; or : or ? or !'
+    fsentence_list = [''.join(tmp[i:i+3]) for i in range(0, len(tmp), 3)]
+    return fsentence_list
+
+
+if __name__ == '__main__':
+    fname = sys.argv[1]
+    fdata = open(fname).read()
+    fsentence_list = create_sentence_list(fdata)
+
+    for fs in fsentence_list:
+        print(fs)
