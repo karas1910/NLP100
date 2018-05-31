e9833cbb0854d54d66c789f485a05bc6e7c2059e
sur1910 <onenight1910@gmail.com>
2018-05-31 23:04:39 +0900

    add 60

---
+++ 7_Chapter/n60.py
@@ -0,0 +1,22 @@
+#
+# usage: python n60 {file name}
+#
+
+
+import sys
+import json
+import plyvel as pl
+
+
+if __name__ == '__main__':
+    fname = sys.argv[1]
+    f = open(fname)
+    f_dictl = [json.loads(line) for line in f]
+    my_db = pl.DB('n60.ldb', create_if_missing=True)
+
+    for f_dict in f_dictl:
+        key = f_dict.get('name')
+        value = f_dict.get('area')
+        aid = str(f_dict.get('id'))
+        if key is not None and value is not None and aid is not None:
+            my_db.put((key+'\s'+aid).encode(), value.encode())
